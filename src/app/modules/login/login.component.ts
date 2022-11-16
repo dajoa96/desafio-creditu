301991ff41237fb8eece7335827a63147fa96594
David Ortega <david96.des@gmail.com>
2022-11-16 09:19:57 -0400

    Se agrego un flujo de pruab al hacer login

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,11 @@ export class LoginComponent implements OnInit {
   isSubmitted: boolean = false;
   showPassword: boolean = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly userService: UserService,     //Only for Testing
+    private readonly router: Router                //Only for Testing
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(32)]]
@@ -29,6 +35,8 @@ export class LoginComponent implements OnInit {
     this.isSubmitted = true;
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
+      this.userService.setToken(JSON.stringify(this.loginForm.value));         //Only for Testing
+      if (this.userService.checkToken()) this.router.navigate(['/home'])       //Only for Testing
     }
   }
 }
