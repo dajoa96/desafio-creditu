474ffb937047f95aec10ac48594fc798e1a53089
David Ortega <david96.des@gmail.com>
2022-11-16 09:19:11 -0400

    Guards, en modo de prueba

--- src/app/guards/logged.guard.ts
+++ src/app/guards/logged.guard.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggedGuard implements CanActivate {
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const resp = this.userService.checkToken() ? true : false;
+    if (resp) {
+      console.log('YA ESTA LOGUEADO, POR FAVOR CERRAR SESION PRIMERO'); //Only For Testing
+      this.router.navigate(['/home']);
+      return false;
+    }
     return true;
   }
-  
+
 }
--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const resp = this.userService.checkToken() ? true : false;
+    if (!resp) {
+      console.log('NO ESTA LOGUEADO, POR FAVOR INICIAR SESION PRIMERO'); //Only For Testing
+      this.router.navigate(['/home']);
+      return false;
+    }
     return true;
   }
 
