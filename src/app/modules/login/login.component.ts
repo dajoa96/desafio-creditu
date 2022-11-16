2fac62fbedd9edb1aa95bea18cdb9b1ee8fcaac6
Aranda Agustin <lunsheygames@gmail.com>
2022-11-15 21:44:40 -0300

    Se genero la base del componente de login

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  isSubmitted: boolean = false;
+  showPassword: boolean = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(32)]]
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  get fc(): any {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    this.isSubmitted = true;
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,9 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
