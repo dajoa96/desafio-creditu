126dad8c6d62ebf202d2e98ff04de689c7ab46b2
David Ortega <david96.des@gmail.com>
2022-11-19 19:42:32 -0400

    Modificaciones a la estructura de login y register

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { first } from 'rxjs';
+import { LoginRequestModel } from 'src/app/models/user-requests.model';
+import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -12,15 +15,17 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   isSubmitted: boolean = false;
   showPassword: boolean = false;
+  errorMessage: string = '';
 
   constructor(
     private readonly fb: FormBuilder,
+    private readonly authService: AuthService,
     private readonly userService: UserService,     //Only for Testing
-    private readonly router: Router                //Only for Testing
+    private readonly router: Router,               //Only for Testing
   ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]],
-      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(32)]]
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(32)]]
     });
   }
 
@@ -32,11 +37,35 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(): void {
+    //We reset the error message
+    this.errorMessage = '';
+    //We confirm the user has tried to submit the form
     this.isSubmitted = true;
+    //Only if the form is valid, we submit
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
-      this.userService.setToken(JSON.stringify(this.loginForm.value));         //Only for Testing
-      if (this.userService.checkToken()) this.router.navigate(['/home'])       //Only for Testing
+      const data: LoginRequestModel = {
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password
+      }
+      this.authService.login(data).pipe(first()).subscribe({
+        next: (res) => {
+          console.log(res);
+        },
+        error: (err) => {
+          console.log(err);
+          switch(err.status) {
+            case 404:
+              this.errorMessage = 'The User/Password is incorret'
+            break;
+            default:
+              this.errorMessage = 'An error has ocurred'
+            break;
+          }
+        }
+      });
+      // this.userService.setToken(JSON.stringify(this.loginForm.value));         //Only for Testing
+      // if (this.userService.checkToken()) this.router.navigate(['/home'])       //Only for Testing
     }
   }
 }
--- src/app/modules/register/register.component.ts
+++ src/app/modules/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { MustMatch } from 'src/app/shared/validators/must-match-validator';
 
 @Component({
   selector: 'app-register',
@@ -9,10 +10,11 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
   loginForm: FormGroup;
   isSubmitted: boolean = false;
   showPassword: boolean = false;
+  showRepeatPassword: boolean = false;
+  errorMessage: string = '';
 
   constructor(
     private readonly fb: FormBuilder,
@@ -21,21 +23,28 @@ export class RegisterComponent implements OnInit {
   ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]],
-      username: ['',[Validators.required, Validators.minLength(4), Validators.maxLength(32)]],
-      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(32)]],
-      repeatPassword: ['']
-    });
+      nickname: ['',[Validators.required, Validators.minLength(4), Validators.maxLength(32)]],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(32)]],
+      repeatPassword: ['', [Validators.required]]
+    },
+    {
+      validator: MustMatch('password', 'repeatPassword')    //this validator is separated due to beign more rehusable, as it can be used with any form controls
+    }
+    );
   }
 
   ngOnInit(): void {
   }
 
+  //Helper to access the form's controls
   get fc(): any {
     return this.loginForm.controls;
   }
 
   onSubmit(): void {
+    //We confirm the user has tried to submit the form
     this.isSubmitted = true;
+    //Only if the form is valid, we submit
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
       this.userService.setToken(JSON.stringify(this.loginForm.value));         //Only for Testing
