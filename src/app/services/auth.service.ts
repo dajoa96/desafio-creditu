7fbec35decb5bd1d4d4fb59c85053f2fe6a956b8
David Ortega <david96.des@gmail.com>
2022-11-19 19:40:13 -0400

    Metodos con api para logear, registar, chequear nickname e email

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { CheckEmailRequestModel, CheckNicknameRequestModel, LoginRequestModel, SignUpRequestModel } from '../models/user-requests.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,19 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
-  authLogin(user: any): Observable<any> {
-    return this.http.post(`${this.API_URL}/login`, user);
+  checkEmail(email: CheckEmailRequestModel): Observable<any>  {
+    return this.http.post(`${this.API_URL}/users/checkEmail`, email);
+  }
+
+  checkNickname(nickname: CheckNicknameRequestModel): Observable<any>  {
+    return this.http.post(`${this.API_URL}/users/checkNickname`, nickname);
+  }
+
+  signUp(user: SignUpRequestModel): Observable<any> {
+    return this.http.post(`${this.API_URL}/users/singUp`, user);
+  }
+
+  login(user: LoginRequestModel): Observable<any> {
+    return this.http.post(`${this.API_URL}/users/login`, user);
   }
 }
