7cfc647cdfd13270e503019e1d200383b162bd5d
David Ortega <david96.des@gmail.com>
2022-11-21 15:16:37 -0400

    Actualizacion de las declaraciones y exportaciones

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,22 +4,31 @@ import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PlayerPagerComponent } from './pager/player-pager/player-pager.component';
+import { SearchDropdownComponent } from './search/search-dropdown/search-dropdown.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    PlayerPagerComponent,
+    SearchDropdownComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
     NgbModule
   ],
   exports: [
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    PlayerPagerComponent,
+    SearchDropdownComponent
   ]
 })
 export class SharedModule { }
