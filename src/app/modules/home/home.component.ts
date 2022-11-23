077881731bbe00ad943d3322139cf6bc870ff9a6
David Ortega <david96.des@gmail.com>
2022-11-23 17:22:37 -0400

    Se actualizo el home, ahora se recibe data real

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, first, firstValueFrom, switchMap } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,32 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   showLoader: boolean = true;
+  bhData: BehaviorSubject<any> = new BehaviorSubject<any>(null);
   data: any = {
-    list: [
-      { id: 1, gameType: 'Rally Racing', nickname: 'jp1995' },
-      { id: 1, gameType: 'Rally Racing', nickname: 'jp1995' },
-      { id: 1, gameType: 'Rally Racing', nickname: 'jp1995' },
-      { id: 1, gameType: 'Rally Racing', nickname: 'jp1995' },
-      { id: 1, gameType: 'Rally Racing', nickname: 'jp1995' },
-      { id: 1, gameType: 'Rally Racing', nickname: 'jp1995' },
-      { id: 1, gameType: 'Rally Racing', nickname: 'jp1995' },
-      { id: 1, gameType: 'Rally Racing', nickname: 'jp1995' },
-      { id: 1, gameType: 'Rally Racing', nickname: 'jp1995' },
-      { id: 1, gameType: 'Rally Racing', nickname: 'jp1995' },
-      { id: 1, gameType: 'Rally Racing', nickname: 'jp1995' }
-    ],
+    list: [],
     pagination: {
       currentPage: 1,
-      totalElements: 15
+      totalElements: 0
     }
   }
 
-  constructor() { }
+  constructor(
+    private readonly userService: UserService
+  ) { }
 
-  ngOnInit(): void {
-    setTimeout(() => {
+  async ngOnInit() {                                                                      //Since we use a await operator we must add the async to the ngOnInit() Method
+    this.showLoader = true;
+    try {
+      const res = await firstValueFrom(this.userService.getTopRanking());                 //Here we use an await promise to simplify the fetching process
+      if (res.status.toLowerCase() === 'success' && Array.isArray(res.data.user)) {       //So we can wait for the data to be available
+        this.data.list = res.data.user;
+        this.data.pagination = this.data.pagination;
+        this.bhData.next(this.data);
+      }
       this.showLoader = false;
-    }, 1500);
+    } catch (error) {
+      this.showLoader = false;
+      console.log(error);
+    }
   }
 
 }
