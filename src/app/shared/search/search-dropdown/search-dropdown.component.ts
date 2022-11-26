b29b91c592a88ad53319a28bece7a587407b5893
David Ortega <david96.des@gmail.com>
2022-11-25 21:36:35 -0400

    Terminado el search bar

--- src/app/shared/search/search-dropdown/search-dropdown.component.ts
+++ src/app/shared/search/search-dropdown/search-dropdown.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject } from 'rxjs';
 import { SearchFilterModel } from 'src/app/models/search-filter.model';
 
 @Component({
@@ -8,15 +9,17 @@ import { SearchFilterModel } from 'src/app/models/search-filter.model';
   templateUrl: './search-dropdown.component.html',
   styleUrls: ['./search-dropdown.component.scss']
 })
-export class SearchDropdownComponent implements OnInit {
+export class SearchDropdownComponent implements OnInit, OnDestroy {
   @Input('redirectOnSearch') redirectOnSearch: boolean = true;
   @Input('filters') filters?: SearchFilterModel[];
   @Output('onNewSearch') onNewSearch: EventEmitter<any> = new EventEmitter<any>();
   currentFilter?: any;
   searchForm: FormGroup;
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(
     private readonly router: Router,
+    private readonly route: ActivatedRoute,
     private readonly fb: FormBuilder
   ) {
     this.searchForm = this.fb.group({
@@ -26,6 +29,24 @@ export class SearchDropdownComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.searchParams()
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  searchParams() {
+    const params: any = this.route.snapshot.queryParams;
+    if (params?.search && params?.search !== '') this.searchForm.get('search')?.setValue(params?.search);
+    if (params?.gameType && params?.gameType !== '') {
+      const found: any = this.filters?.slice().find((f) => f.value === params?.gameType?.toLowerCase());
+      if (found) {
+        this.currentFilter = found;
+        this.searchForm.get('gameType')?.setValue(params?.gameType);
+      }
+    }
   }
 
   onSearch() {
