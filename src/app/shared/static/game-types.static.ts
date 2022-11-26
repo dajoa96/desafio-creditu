2098dc021e3a374e94fbb509064556b9f9da8972
David Ortega <david96.des@gmail.com>
2022-11-25 21:35:32 -0400

    Archivo con las definiciones de game type

---
+++ src/app/shared/static/game-types.static.ts
@@ -0,0 +1,17 @@
+export const GameTypesStatic: any [] = [
+  {
+    label: 'Street Racing',
+    gameType: 'street-racing',
+    image: '/assets/images/user/rally.png'
+  },
+  {
+    label: 'Track Racing',
+    gameType: 'track-racing',
+    image: '/assets/images/user/f1.png'
+  },
+  {
+    label: 'Rally Racing',
+    gameType: 'rally-racing',
+    image: '/assets/images/user/rally.png'
+  }
+];
