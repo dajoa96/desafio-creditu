cd32ce782cb807d59e26dba1faa978b31b4d32b9
David Ortega <david96.des@gmail.com>
2022-11-29 03:04:32 -0400

    Arreglado el link de la API en produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api: {
-    link: 'https://api-kraken-race.krakentechstudios.com/'
+    link: 'https://api-kraken-race.krakentechstudios.com'
   }
 };
